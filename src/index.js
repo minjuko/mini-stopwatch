c76edcf14e339e4f84eb5d83f1adb5c060686478
minjuko <rhalswn0526@gmail.com>
2024-01-27 22:31:47 +0900

    feat: 시작/중단 기능

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-// TODO: 이 곳에 정답 코드를 작성해주세요.
+import Stopwatch from "./stopwatch";
+
+const stopwatch = new Stopwatch();
+
+let isRunning = false;
+let interval;
+
+const $timer = document.getElementById('timer');
+
+const $lapResetBtn = document.getElementById('lap-reset-btn');
+const $startStopBtn = document.getElementById('start-stop-btn');
+const $lapResetBtnLabel = document.getElementById('lap-reset-btn-label');
+const $startStopBtnLabel = document.getElementById('start-stop-btn-label');
+const $laps = document.getElementById('laps');
+
+let $minLap, $maxLap;
+
+const updateTime = (time) => {
+    $timer.innerText = time;
+};
+
+const toggleBtnStyle = () => {
+    $startStopBtn.classList.toggle('bg-green-600');
+    $startStopBtn.classList.toggle('bg-red-600');
+};
+
+const onClickStartStopBtn = () => {
+    if (isRunning) {
+        onClickStopBtn();
+    } else {
+        onClickStartBtn();
+    }
+    isRunning = !isRunning;
+    toggleBtnStyle();
+};
+const onClickStartBtn = () => {
+    stopwatch.start();
+    interval = setInterval(() => {
+        updateTime(stopwatch.centisecond);
+    }, 10);
+    $lapResetBtnLabel.innerText = '랩';
+    $startStopBtnLabel.innerText = '중단';
+};
+
+const onClickStopBtn = () => {
+    stopwatch.pause();
+    clearInterval(interval);
+    $lapResetBtnLabel.innerText = '리셋';
+    $startStopBtnLabel.innerText = '시작';
+};
+
+$startStopBtn.addEventListener('click', onClickStartStopBtn);
+
