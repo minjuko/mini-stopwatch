3c1154dd77774fc16486b7c932a5eb7025674091
minjuko <rhalswn0526@gmail.com>
2024-01-27 22:33:08 +0900

    feat: 시간 포맷팅

--- src/index.js
+++ src/index.js
@@ -15,8 +15,21 @@ const $laps = document.getElementById('laps');
 
 let $minLap, $maxLap;
 
+const formatString = (num) => (num < 10 ? '0' + num : num);
+
+const formatTime = (centisecond) => {
+    let formattedString = '';
+    const min = parseInt(centisecond / 6000);
+    const sec = parseInt((centisecond - 6000 * min) / 100);
+    const centisec = centisecond % 100;
+    formattedString = `${formatString(min)}:${formatString(sec)}.${formatString(
+        centisec
+    )}`;
+    return formattedString;
+};
+
 const updateTime = (time) => {
-    $timer.innerText = time;
+    $timer.innerText = formatTime(time);
 };
 
 const toggleBtnStyle = () => {
